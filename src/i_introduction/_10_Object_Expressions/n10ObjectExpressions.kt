b211aa9611b6af12fb7dc47524a4e71e643239cb
dsucksto <dsucksto@thoughtworks.com>
2017-12-05 22:52:53 -0500

    Intellij was too good to me =[

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -4,10 +4,12 @@ import java.util.*
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList) { o1, o2 ->
-        if (o1 == null) {
-            if (o2 == null) 0 else 1
-        } else o2?.compareTo(o1) ?: 1
-    }
+    Collections.sort(arrayList, object : java.util.Comparator<Int> {
+        override fun compare(o1: Int?, o2: Int?): Int {
+            return if (o1 == null) {
+                if (o2 == null) 0 else 1
+            } else o2?.compareTo(o1) ?: 1
+        }
+    })
     return arrayList
 }
