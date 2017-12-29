2909016f010815ca35160eaaa5b4b03270acdd31
dsucksto <dsucksto@thoughtworks.com>
2017-12-29 09:11:51 -0600

    Koan 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            other.year == year && other.month == month && other.dayOfMonth == dayOfMonth -> 0
+            other.year <= year && other.month <= month && other.dayOfMonth < dayOfMonth -> 1
+            else -> -1
+        }
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
--- test/iii_conventions/N25ComparisonKtTest.kt
+++ test/iii_conventions/N25ComparisonKtTest.kt
@@ -22,6 +22,12 @@ class N25ComparisonKtTest {
         assertTrue("The date ${first.s} should be after ${second.s}", first > second)
     }
 
+    @Test fun testBeforeEdgeCase() {
+        val first = MyDate(2013, 8, 12)
+        val second = MyDate(2014, 7, 11)
+        assertTrue("The date ${first.s} should be before ${second.s}", first < second)
+    }
+
     /* If you declare 'compareTo' as an extension function, remove this one to make the code compile */
     operator fun MyDate.compareTo(other: MyDate): Int = todoTask25()
 }
